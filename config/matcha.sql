115b502d4190f88c4e554f7ab5b124ea0c3bf0cf
Tommy PAGEARD <tommy.pageard@student.42.fr>
2017-05-20 15:00:52 +0200

    update db for email validation

--- config/matcha.sql
+++ config/matcha.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2016 at 02:08 PM
+-- Generation Time: May 19, 2017 at 03:38 PM
 -- Server version: 5.7.11
 -- PHP Version: 7.0.0
 
@@ -27,14 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(256) NOT NULL,
   `username` varchar(21) NOT NULL,
-  `password` varchar(256) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `lastname` varchar(20) NOT NULL,
   `birthdate` date NOT NULL,
-  `gender` tinyint(1) NOT NULL
+  `gender` tinyint(1) NOT NULL,
+  `email_validated` tinyint(1) NOT NULL,
+  `token_email` varchar(60) DEFAULT NULL,
+  `token_password` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
@@ -55,7 +58,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
