ec37c4c943926ea26ca96b67ab2ea6c9f97d73c2
Tommy PAGEARD <tommy.pageard@student.42.fr>
2017-05-22 14:58:42 +0200

    user route update & add profile routes

--- routes/render.js
+++ routes/render.js
@@ -2,17 +2,32 @@
 
 exports.home = {
 
-	index: function( req, res) {
+	index: function(req, res) {
 		res.render('index', { title: 'Matcha', authorized: req.checkAuth });
 	}
 }
 
 exports.user = {
 
-	register: function( req, res) {
+	register: function(req, res) {
 		res.render('user/new', { title: 'New User', authorized: req.checkAuth });
   },
-  login: function( req, res ) {
+  login: function(req, res ) {
     res.render('user/login', { title: 'Matcha Login', authorized: req.checkAuth });
-  }
+  },
+	me: function(req, res) {
+		res.render('user/me', {title: "My Account", authorized: req.checkAuth });
+	}
+}
+
+exports.profile = {
+	user: function(req, res) {
+		res.render('profile/user', {title: "User profile", authorized: req.checkAuth });
+	},
+	users: function(req, res) {
+		res.render('profile/users', {title: "Users search", authorized: req.checkAuth });
+	},
+	edit: function(req, res) {
+		res.render('profile/edit', {title: "Editing profile", authorized: req.checkAuth });
+	}
 }
--- routes/routes.js
+++ routes/routes.js
@@ -22,14 +22,28 @@ routes.user = require('./user');
 router.get('/', checkAuth, routes.render.home.index);
 
 /* User rooting */
+
+// creating new user
 router.get('/user/new', checkAuth, routes.render.user.register);
 router.post('/user/new', checkAuth, routes.user.register);
 
+// user account information
+router.get('/user/me', checkAuth, routes.render.user.me);
+
+// user email validation
 router.get('/user/emailvalidation', checkAuth, routes.user.emailvalidation);
 
+// user login / logout
 router.get('/user/login', checkAuth, routes.render.user.login);
 router.post('/user/login', checkAuth, routes.user.login);
-
 router.get('/user/logout', checkAuth, routes.user.logout);
+
+/* Profile rooting */
+
+// user profile
+router.get('/profile/:username', checkAuth, routes.render.profile.user)
+router.get('/profile/:username/edit', checkAuth, routes.render.profile.edit)
+// users profiles
+router.get('/profile/', checkAuth, routes.render.profile.users);
 
 module.exports = router;
