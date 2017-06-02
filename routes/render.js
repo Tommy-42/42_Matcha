c9511361814748309f93af9590d7c78fb57abc42
Tommy PAGEARD <tommy.pageard@student.42.fr>
2017-06-02 10:06:19 +0200

    remove console.log

--- routes/render.js
+++ routes/render.js
@@ -1,14 +1,13 @@
 "use strict";
 
 exports.home = {
-
 	index: function(req, res) {
+		console.log(req.session);
 		res.render('index', { title: 'Matcha', authorized: req.checkAuth });
 	}
 }
 
 exports.user = {
-
 	register: function(req, res) {
 		res.render('user/new', { title: 'New User', authorized: req.checkAuth });
   },
