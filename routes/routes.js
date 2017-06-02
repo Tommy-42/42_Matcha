5cec5bea00d85f3fa4d8eaf239a7b8dafabc1ff1
Tommy PAGEARD <tommy.pageard@student.42.fr>
2017-06-02 10:06:53 +0200

    add profile route user route modified auth middleware

--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,14 @@ var router = express.Router();
 // midleware checking if user is log
 function checkAuth(req, res, next) {
 	console.log(req.session);
-    if ( req.session.user_id != 1 ) {
-      req.checkAuth = false;
-      next();
+    if ( req.session && req.session.user_id == null ) {
+      res.render('error', {
+        title: 'New User',
+        authorized: false,
+        msg: 'You need to log in first',
+        msg_detailed: 'You will be redirected shortly.',
+        script: 'window.setTimeout(function(){ window.location.href = window.location.origin + "/user/login"; }, 3000);'
+      });
     } else {
       req.checkAuth = true;
       next();
@@ -19,23 +24,23 @@ routes.render = require('./render');
 routes.user = require('./user');
 
 /* Home rooting */
-router.get('/', checkAuth, routes.render.home.index);
+router.get('/', routes.render.home.index);
 
 /* User rooting */
 
 // creating new user
-router.get('/user/new', checkAuth, routes.render.user.register);
-router.post('/user/new', checkAuth, routes.user.register);
+router.get('/user/new', routes.render.user.register);
+router.post('/user/new', routes.user.register);
 
 // user account information
 router.get('/user/me', checkAuth, routes.render.user.me);
 
 // user email validation
-router.get('/user/emailvalidation', checkAuth, routes.user.emailvalidation);
+router.get('/user/emailvalidation', routes.user.emailvalidation);
 
 // user login / logout
-router.get('/user/login', checkAuth, routes.render.user.login);
-router.post('/user/login', checkAuth, routes.user.login);
+router.get('/user/login', routes.render.user.login);
+router.post('/user/login', routes.user.login);
 router.get('/user/logout', checkAuth, routes.user.logout);
 
 /* Profile rooting */
